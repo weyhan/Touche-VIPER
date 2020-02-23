dab2ed4f2bd1d5dff60e08a9c83de9bcd6f3b028
weyhan <weyhan@gmail.com>
2020-02-23 18:03:22 +0800

    Initial Commit

---
+++ Touche/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Touche-VIPER
+//
+//  Created by weyhan.
+//  Copyright © 2020 eccentricdeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
---
+++ Touche/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Touche-VIPER
+//
+//  Created by weyhan.
+//  Copyright © 2020 eccentricdeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
